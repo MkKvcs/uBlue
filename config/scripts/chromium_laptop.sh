6ca119a3838136f5d48338ffed49e4c54fb023bc
MkKvcs <148012377+MkKvcs@users.noreply.github.com>
2024-06-25 00:20:15 +0200

    Update chromium_laptop.sh

--- config/scripts/chromium_laptop.sh
+++ config/scripts/chromium_laptop.sh
@@ -2,3 +2,4 @@
 set -oue pipefail
 sed -i 's/GRAPHIC_DRIVER=default/GRAPHIC_DRIVER=intel/' /etc/chromium/chromium.conf
 sed -i 's/--enable-features=/--enable-features=TouchpadOverscrollHistoryNavigation,/' /etc/chromium/chromium.conf
+sed -i '/"NetworkServiceSandboxEnabled": true/d' /etc/chromium/policies/managed/hardening.json 
