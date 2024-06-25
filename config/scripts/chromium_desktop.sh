1687eb23b1b883b846f4a3ecf6dd11f06a42e372
MkKvcs <kovacsmiklos@pm.me>
2024-06-25 02:46:28 +0200

    Add Rider

--- config/scripts/chromium_desktop.sh
+++ config/scripts/chromium_desktop.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 set -oue pipefail
 sed -i 's/GRAPHIC_DRIVER=default/GRAPHIC_DRIVER=amd/' /etc/chromium/chromium.conf
+sed -i '/"NetworkServiceSandboxEnabled": true/d' /usr/etc/chromium/policies/managed/hardening.json
+
 
