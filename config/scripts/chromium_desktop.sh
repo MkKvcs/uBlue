04a2b86177114e75e9ccb3dfe2209c66b7a3a036
MkKvcs <148012377+MkKvcs@users.noreply.github.com>
2024-07-29 14:55:12 +0200

    Update chromium_desktop.sh

--- config/scripts/chromium_desktop.sh
+++ config/scripts/chromium_desktop.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 set -oue pipefail
 sed -i 's/GRAPHIC_DRIVER=default/GRAPHIC_DRIVER=amd/' /etc/chromium/chromium.conf
-sed -i '/"NetworkServiceSandboxEnabled": true/d' /usr/etc/chromium/policies/managed/hardening.json
-
-
